55cce32d35b6c62aef7c5829f1c71767020d4bdc
nikhilkumbhare <nikhilk190697@gmail.com>
2021-06-17 22:50:45 +0530

    Added: OpenID Connect Implementation

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.redirectUri = window.location.origin;
+    this.oauthService.clientId = '0oa11636v6fMpOTGM5d7';
+    this.oauthService.scope = 'openid profile email';
+    this.oauthService.issuer = 'https://dev-05228358.okta.com/oauth2/default';
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+
+    // Load Discovery Document and then try to login the user
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+}
